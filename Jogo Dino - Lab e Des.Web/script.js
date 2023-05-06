55799022c2b2fc202cc57cae0122b18a42e5dea4
GFutureTI <125501234+GFutureTI@users.noreply.github.com>
2023-05-06 08:00:08 -0300

    Update script.js

--- Jogo Dino - Lab e Des.Web/script.js
+++ Jogo Dino - Lab e Des.Web/script.js
@@ -1,38 +1,39 @@
-//variaveis que controlam os eventos do personagem e do quadrado
+// variáveis que controlam os eventos do personagem e do quadrado
 let personagem = document.querySelector('#personagem')
 let quadrado = document.querySelector('#quadrado')
 
-//cria funçao com uma condição de fazer controlar o pulo do personagem 
-function pular(){
-    if(personagem.classList != 'animar'){
-
+// cria função para controlar o pulo do personagem
+function pular() {
+    if(personagem.classList != 'animar') {
         personagem.classList.add('animar')
     }
 
-
-    setTimeout(function(){
-        /*Depois de passados 500ms a classe será removida e só retornará a ser adicionada se o usuario clicar com o mouse na tela do jogo */
-
+    setTimeout(function() {
         personagem.classList.remove('animar')
     }, 500)
-
 }
 
-//cria a funçao de colisao do obstaculo
-var testarColisao = setInterval(function(){
+// adiciona um ouvinte de evento para detectar a tecla espaço pressionada *
+document.addEventListener('keydown', function(event) {
+    if(event.code === 'Space') {
+        pular()
+    }
+})
 
-// declaraçao das variaveis a serem controladas na estrutura condicional IF
+// cria função de colisão do obstáculo
+var testarColisao = setInterval(function() {
     var topoPersonagem = parseInt(
         window.getComputedStyle(personagem).getPropertyValue('top')
     )
     var EsquerdaQuadrado = parseInt(
         window.getComputedStyle(quadrado).getPropertyValue('left')
     )
-/*Estrutura condicional que realizará a checagem periodica se o obstaculo colidiu com o quadrado realizando assim o "Game Over" que é sua condição final*/
-    if(EsquerdaQuadrado < 20 && EsquerdaQuadrado > 0 && topoPersonagem >= -50){
+
+    if(EsquerdaQuadrado < 20 && EsquerdaQuadrado > 0 && topoPersonagem >= -50) {
         quadrado.style.animation = 'none'
         quadrado.style.display = 'none'
         alert('Fim de Jogo')
     }
-
 }, 10)
+
+

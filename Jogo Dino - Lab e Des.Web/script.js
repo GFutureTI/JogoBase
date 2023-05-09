8d70435c0f474f6bfd63a06cf750f3aa3b15ada2
GFutureTI <125501234+GFutureTI@users.noreply.github.com>
2023-05-09 18:15:46 -0300

    Update script.js

--- Jogo Dino - Lab e Des.Web/script.js
+++ Jogo Dino - Lab e Des.Web/script.js
@@ -1,6 +1,6 @@
-// variáveis que controlam os eventos do personagem e do quadrado
 let personagem = document.querySelector('#personagem')
 let quadrado = document.querySelector('#quadrado')
+let pontuacao = 0 // variável para armazenar a pontuação atual
 
 // cria função para controlar o pulo do personagem
 function pular() {
@@ -13,11 +13,16 @@ function pular() {
     }, 500)
 }
 
-// adiciona um ouvinte de evento para detectar a tecla espaço pressionada *
+// adiciona um ouvinte de evento para detectar a tecla espaço pressionada
 document.addEventListener('keydown', function(event) {
     if(event.code === 'Space') {
         pular()
     }
+})
+
+// adiciona um ouvinte de evento de clique no botão de reiniciar
+document.querySelector('#reiniciar').addEventListener('click', function() {
+    location.reload() // recarrega a página para reiniciar o jogo
 })
 
 // cria função de colisão do obstáculo
@@ -32,8 +37,11 @@ var testarColisao = setInterval(function() {
     if(EsquerdaQuadrado < 20 && EsquerdaQuadrado > 0 && topoPersonagem >= -50) {
         quadrado.style.animation = 'none'
         quadrado.style.display = 'none'
-        alert('Fim de Jogo')
+        clearInterval(testarColisao) // interrompe a verificação de colisão
+        alert('Fim de Jogo\nVocê fez ' + pontuacao + ' pontos!')
+    } else {
+        // se não houve colisão, incrementa a pontuação e atualiza a placa
+        pontuacao++
+        document.getElementById('placar').innerHTML = 'Pontuação: ' + pontuacao
     }
-}, 10)
-
-
+}, 40)
